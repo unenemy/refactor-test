be978689b4717f26aaa80fe9e9342a811996f355
bogdasya <bogdasya93@gmail.com>
2015-12-17 14:41:24 +0200

    Refactoring Service

--- app/services/translate_service.rb
+++ app/services/translate_service.rb
@@ -1,9 +1,16 @@
 class TranslateService
+  SITE_URL = "https://translate.yandex.net/api/v1.5/tr.json/translate?"
 
   def self.translate_text(content, translate_to)
-    url = "https://translate.yandex.net/api/v1.5/tr.json/translate?"
-    [JSON.parse(HTTParty.get(URI.escape("#{url}key=#{Rails.application.secrets.yandex_key}&text=#{content.title}&lang=#{translate_to}")).body)['text'].join,
-      JSON.parse(HTTParty.get(URI.escape("#{url}key=#{Rails.application.secrets.yandex_key}&text=#{content.content}&lang=#{translate_to}")).body)['text'].join ]
+    [ parse_answer(create_url(content.title, translate_to)), parse_answer(create_url(content.content, translate_to)) ]
+  end
+
+  def self.create_url(content, translate_to)
+    HTTParty.get(URI.escape("#{SITE_URL}key=#{Rails.application.secrets.yandex_key}&text=#{content}&lang=#{translate_to}"))
+  end
+
+  def self.parse_answer(response)
+    JSON.parse(response.body)['text'].join
   end
 
 end

711f6d821961b0219a5c2c59f14b96c3cb124f49
bogdasya <bogdasya93@gmail.com>
2015-12-17 14:46:24 +0200

    Refactoring Service

---
+++ app/models/global_constants.rb
@@ -0,0 +1,3 @@
+module GlobalConstants
+  SITE_URL = ["https://translate.yandex.net/api/v1.5/tr.json/translate?"].freeze
+end
--- app/services/translate_service.rb
+++ app/services/translate_service.rb
@@ -1,12 +1,11 @@
 class TranslateService
-  SITE_URL = "https://translate.yandex.net/api/v1.5/tr.json/translate?"
 
   def self.translate_text(content, translate_to)
     [ parse_answer(create_url(content.title, translate_to)), parse_answer(create_url(content.content, translate_to)) ]
   end
 
   def self.create_url(content, translate_to)
-    HTTParty.get(URI.escape("#{SITE_URL}key=#{Rails.application.secrets.yandex_key}&text=#{content}&lang=#{translate_to}"))
+    HTTParty.get(URI.escape("#{GlobalConstants::SITE_URL[0]}key=#{Rails.application.secrets.yandex_key}&text=#{content}&lang=#{translate_to}"))
   end
 
   def self.parse_answer(response)
